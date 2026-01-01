a286e80b7eb8522bfc099bcb2d4b9a23be612bab
Francisco Milanich <franciscomilanich@gmail.com>
2026-01-01 03:52:53 -0300

    syntax change

--- 2015/1. Not Quite Lisp/part2.cs
+++ 2015/1. Not Quite Lisp/part2.cs
@@ -1,16 +1,14 @@
 
 string s = File.ReadLines("input.txt").First();
 
-int result = 0;
-int pos = 0;
-int i = 0;
+int result = 0, pos = 0, i = 0;
 
 while(pos != -1)
 {
     char paren = s[i];
 
     if(paren == '(') pos++;
-    else pos--;
+    else if(paren == ')') pos--;
 
     result++;
     i++;
